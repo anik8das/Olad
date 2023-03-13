cc9fb003cb58fa4a3ca6d7e8c58e45983dd6bc3c
Aniket Das <aniketdas001@gmail.com>
2023-03-13 15:45:43 +0800

    Updated content of About page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -15,33 +15,31 @@ export default function About() {
 			<Container className="w-50 pt-5 pb-5">
 				<div className="h1 mb-4">About Us</div>
 				<div className="fs-5 fw-light mb-5">
-					A third-party organization or mechanism, focussed
-					exclusively on peer-review and fully responsible for it —
-					from selecting eligible reviewers to sending back a
-					completely annotated paper back to the journal. The process
-					overview is described in Figure 2. It will recruit trained
-					scientists, provide training to the inexperienced ones,
-					utilize a Skepsi-like software for the reviewing process,
-					match papers to relevant reviewers, pay them the fee after
-					deducting administrative costs, conduct randomized quality
-					control checks, maintain a reviewer index for reliability
-					statistics, and allow the author as well as the editor to
-					contribute or clarify during the process.
+					We're a third-party organization focussed exclusively on
+					peer-review and fully responsible for it — from selecting
+					eligible reviewers to sending back a completely annotated
+					and professionally formatted paper back to journals. We have
+					a pool of trained scientists and researchers. Efficiency and
+					accuracy is enhanced by utilizing a annotation-based
+					collaborative software for the reviewing process. Quality is
+					ensured by maintaining a reviewer index and randomized audit
+					checks. Lastly, but most importantly, we allow journals to
+					focus on their core value proposition: selectivity,
+					curation, and a robust submission and feedback system.
 				</div>
 				<div className="h1 mb-4">Why does it work?</div>
 				<div className="fs-5 fw-light mb-5">
-					A third-party organization or mechanism, focussed
-					exclusively on peer-review and fully responsible for it —
-					from selecting eligible reviewers to sending back a
-					completely annotated paper back to the journal. The process
-					overview is described in Figure 2. It will recruit trained
-					scientists, provide training to the inexperienced ones,
-					utilize a Skepsi-like software for the reviewing process,
-					match papers to relevant reviewers, pay them the fee after
-					deducting administrative costs, conduct randomized quality
-					control checks, maintain a reviewer index for reliability
-					statistics, and allow the author as well as the editor to
-					contribute or clarify during the process.
+					Centralization leads to a plethora of benefits. Training and
+					collaboration ensures consistency and standardization.
+					Forced double blinding removes any potential for biases. A
+					concentrated single pool of reviewers results in faster and
+					better matches. We also maintain a financial pool to help
+					financially need reviewers and reduce demographic bias in
+					the peer-review process. Eliminating personal relationships
+					between authors, reviewers, and journals helps cut
+					corruption and biases. Lastly, it frees up resources for
+					journals to focus on buiilding better systems for accepting
+					and maintaining papers.
 				</div>
 				<div className="h1 mb-4">Learn more</div>
 				<div className="fs-5 fw-light">
